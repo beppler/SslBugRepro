9e4fa3401a9e575bc951d304e40a1981e4a56127
Carlos Alberto Costa Beppler <carlos.beppler@mps.com.br>
2019-05-22 11:53:57 -0300

    Add project files.

---
+++ SslBugRepro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+
+namespace SslBugRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MakeRequest("github.com", 443);
+                MakeRequest("git.mps.com.br", 443);
+                MakeRequest("api.integracao-bnmp.cnj.jus.br", 443);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static void MakeRequest(string host, int port)
+        {
+            Console.WriteLine($"Connecting to {host}:{port}");
+            using (var tcpClient = new TcpClient())
+            {
+                tcpClient.Connect(host, port);
+                using (var tcpStream = tcpClient.GetStream())
+                {
+                    using (var stream = new SslStream(tcpStream, false))
+                    {
+                        stream.AuthenticateAsClient(host, null, SslProtocols.Tls12, false);
+                        Console.WriteLine($"\tSSL Protocol: {stream.SslProtocol}");
+                        Console.WriteLine($"\tCipher Algorithm: {stream.CipherAlgorithm}");
+                        Console.WriteLine($"\tCipher Strength: {stream.CipherStrength}");
+                        Console.WriteLine($"\tHash Algorithm: {stream.HashAlgorithm}");
+                        Console.WriteLine($"\tHash Strength: {stream.HashStrength}");
+                        Console.WriteLine($"\tKey Exchange Algorithm: {stream.KeyExchangeAlgorithm}");
+                        Console.WriteLine($"\tKey Exchange Strength: {stream.KeyExchangeStrength}");
+                        Console.WriteLine($"\tCertificate:\n\t\t{string.Join("\n\t\t", stream.RemoteCertificate.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))}");
+                    }
+                }
+            }
+        }
+    }
+}
